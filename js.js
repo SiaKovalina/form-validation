8fa2793214975a7daca9c51853551ae8569c9d3e
SiaKovalina <siakovalina@gmail.com>
2018-05-04 14:28:57 +0300

    fixed date validation

--- js.js
+++ js.js
@@ -33,8 +33,25 @@ var formEl = document.createElement('form');
             var regexName = /^user_+/;
             var regexDate = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
 
-            if (regexAge.test(ageInput.value) || !regexName.test(nameInput.value) || !regexDate.test(dateInput.value)) {
-                alert('your data is invalid');
-            };
+            dateArr = dateInput.value.split('/');
+            console.log(dateArr);
+            var day = parseInt(dateArr[0], 10);
+            console.log(day);
+            var month = parseInt(dateArr[1], 10);
+            var year = parseInt(dateArr[2], 10);
+            console.log(year);
 
+            var months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+            if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+                months[1] = 29;
+            }
+
+            if (regexAge.test(ageInput.value) || !regexName.test(nameInput.value) || !regexDate.test(dateInput.value) || year < 1900 || year > 2200 || month < 1 || month > 12 || day < 1 || day > months[month - 1]) {
+                alert('your data is invalid');
+            } 
         }
+
+        
+
+        
